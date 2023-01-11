1379a4af6e8658cf39fe29e6f99ffdd68e5f8573
akbulut99 <akbulut1199@gmail.com>
2023-01-11 17:57:26 +0300

    numpy4

---
+++ numpy4.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+ar1 = np.arange(0,10,2)
+ar2 = np.arange(0,13,3)
+print(ar1+ar2) # iki np arrayın toplamı
+print(ar1-ar2) # iki np arrayın çıkartılması
+print(ar1*ar2) # iki np arrayın iç çarpımı
+print(ar1+10)  # np arrayi int değerle toplama
+print(ar2/3)   # np arrayi int değere bölme
