764cf546a02c036e3482f709dd9efbe4a7fcd73d
akbulut99 <akbulut1199@gmail.com>
2023-01-11 20:41:22 +0300

    pandas1

--- pandas1.py
+++ pandas1.py
@@ -1,1 +1,11 @@
-print("  ")
+import numpy as np
+import pandas as pd 
+label_list=["mustafa","kemal","murat","kadir","zeynep"]
+data_list =[10,20,30,40,50]
+a = pd.Series(data= data_list,index= label_list) #pandas serimizi oluşturduk
+b = pd.Series(data_list) #herhangi bir index vermezsek kendisi otomatik atar
+nparray = np.array([10,20,30,40,50])
+print(pd.Series(nparray,label_list))#np listesi de verebiliriz
+print(pd.Series(data= nparray,index=["a","b","c","d","e "]))
+datadic ={"kadir":30,"murat":58,"cenk":60} #verilerimizi sözlük formatında oluşturduk
+print(pd.Series(datadic)) 
