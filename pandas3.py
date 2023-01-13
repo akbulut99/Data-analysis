79ffb8b7eb4181b02853a67bb9438ff9d876451c
akbulut99 <akbulut1199@gmail.com>
2023-01-13 16:20:57 +0300

    pandas3

--- pandas3.py
+++ pandas3.py
@@ -1,6 +1,11 @@
 import numpy as np
 import pandas as pd 
-from numpy.random import rand
-df = pd.DataFrame(rand(4,3),["A","B","C","D"], ["Column1","Column2","Column3"])
-print(df)
+from numpy.random import randn
+df = pd.DataFrame(randn(4,3),["A","B","C","D"], ["Column1","Column2","Column3"])
 print(df["Column1"] > 0)#numpylarda filtreleme işlemeni pandasda da yapabiliyoruz
+df["Column4"] = pd.Series(randn(4),["A","B","C","D"]) #yeni bir sütun ekliyoruz
+print(df)
+df["Column5"] = randn(4) # başka bir sütun eklme yöntemi
+print(df)
+df["Column6"] = ["newValue1","newValue2","newValue3","newValue4"]
+df.set_index("Column6")
