c017d6857bcb0ed4132f8741b7f399eb053854cd
akbulut99 <akbulut1199@gmail.com>
2023-01-13 15:33:57 +0300

    pandas2

--- pandas2.py
+++ pandas2.py
@@ -1,9 +1,14 @@
+
 import numpy as np 
 import pandas as pd 
-from numpy.random import randn
+from numpy.random import randn,rand
 
-df = pd.DataFrame(data= randn(3,3),index =["a","b","c"],columns=["sütun1","sütun2","sütun3"])
+df = pd.DataFrame(data= randn(3,3), index = ["A","B","C"], columns = ["sütun1","sütun2","sütun3"])
 #dataframeler serilerin birleşmiş halidir
-print(df("sütun2"))#2.sütuna karşılık gelen değerleri gösterir
-
- 
+print(df["sütun1"])
+df["sütun4"] = pd.Series(randn(3),["A","B","C"])#frame'mize yeni bir sütun ekledik
+print(df)
+df.drop("sütun4",axis=1,inplace=True)#"1.satıra göre sütun 4'ü siler"
+#dataframe'i güncellemek için inplace=True olarak değeri vermeliyiz..a
+print(df)
+print(df.loc["A"])
