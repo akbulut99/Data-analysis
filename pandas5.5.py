2c3cabeb4cd0c1fe4ddf5a89fb88f3ea2e4db542
akbulut99 <akbulut1199@gmail.com>
2023-01-16 15:11:25 +0300

    pandas5.5

--- pandas5.5.py
+++ pandas5.5.py
@@ -10,4 +10,5 @@ def calculateMean(df):
 arr = np.array([[10,20,np.nan],[5,np.nan,np.nan],[21,np.nan,10]]) # 3x3 lük bir matrsi oluşturduk
 df = pd.DataFrame(arr,index=["Index1","Index2","Index3"],columns=["Column1","Column2","Column3"])
 
-print(df.fillna(value= calculateMean(df)))
+print(df.fillna(value= calculateMean(df),inplace=True)) #inplace ile matrisimizi güncelledik
+print(df)
