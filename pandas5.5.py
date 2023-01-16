09331dead43ca693c6d0415d51c50f77d8077afa
akbulut99 <akbulut1199@gmail.com>
2023-01-16 15:09:47 +0300

    pandas5.5

---
+++ pandas5.5.py
@@ -0,0 +1,13 @@
+import numpy as np 
+import pandas as pd 
+from numpy.random import randn
+
+def calculateMean(df):
+ totalsum = df.sum().sum()  
+ totalnum = df.size - df.isnull().sum().sum()
+ return totalsum / totalnum
+
+arr = np.array([[10,20,np.nan],[5,np.nan,np.nan],[21,np.nan,10]]) # 3x3 lük bir matrsi oluşturduk
+df = pd.DataFrame(arr,index=["Index1","Index2","Index3"],columns=["Column1","Column2","Column3"])
+
+print(df.fillna(value= calculateMean(df)))
