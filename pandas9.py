108978224a803790350cfc7f09c1bc78812865f5
akbulut99 <akbulut1199@gmail.com>
2023-01-17 15:32:53 +0300

    pandas9

---
+++ pandas9.py
@@ -0,0 +1,9 @@
+import pandas as pd 
+dataset = pd.read_csv("USvideos.csv")
+newdateset1 = dataset.drop(["video_id","trending_date"],axis = 1) #video_id,trending_date columnlarını silidik 
+newdateset1.to_csv("USvideos1.csv",index = False) #elde ettiğimiz date seti yeni bir csv dosyası olarak yazıyoruz
+  
+excelset = pd.read_excel("excelfile.xlsx") #excel dosyası olarak kaydettik
+excelset["Column5"] = ["Mustafa","Murat","Coşkun","Udemy"]
+excelset.to_excel("newexcelfile.xlsx ")
+
