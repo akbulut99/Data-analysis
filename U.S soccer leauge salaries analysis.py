1fa4a1c3ab29016eec74d521929ad91a36aa8db8
akbulut99 <akbulut1199@gmail.com>
2023-01-18 21:32:25 +0300

    salaries

--- U.S soccer leauge salaries analysis.py
+++ U.S soccer leauge salaries analysis.py
@@ -10,7 +10,12 @@ soccer[soccer["last_name"]=="Gonzalez Pirez"]["position"].iloc[0]
 #gonzalez pirez ismindeki oyuncunun pozisyon bilgisini verir
 soccer.groupby("position").mean()#oyuncuları pozisyona göre gruplayın ortalama maaşlarını buluyoruz
 soccer["position"].unique()#kaç farklı posizyon olduğunu gösterir
+soccer["position"].value_counts()#her mevkide kaç oyuncu olduğunu value_counts() ile bulduk
+soccer["club"].value_counts()#her takımda kaç oyuncu oynadığını saydırdık
 
+def re_find(last_name): #soyadında 'son' geçen futbolcuları bulmamızı sağlayan fonk
+    if "son" in last_name.lower():
+        return True
+    return False
 
-
-
+soccer[soccer["last_name"].apply(re_find)]
