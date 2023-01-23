644486ca0471531c64fe33333a97b488b6ba10af
akbulut99 <akbulut1199@gmail.com>
2023-01-23 15:34:13 +0300

    youtube

--- youtube.py
+++ youtube.py
@@ -9,8 +9,15 @@ youtube["dislikes"].mean()
 youtube[youtube["views"].max() == youtube["views"]]["title"].iloc[0]#en yüksek görüntülemeyi alan video bilgisi
 youtube[youtube["views"].min()==youtube["views"]]["title"].iloc[0]#en düşük görüntülemeyi alan video bilgisi
 youtube.groupby("category_id").mean()[["comment_count"]] #kategorilere göre yorum sayılarının ortalamasını bulduk
+youtube["category_id"].value_counts()
+def countTitleCount(title):
+    return len(title)
+youtube["title_length"] = youtube["title"].apply(countTitleCount)
 
-
+def countTag(tags):
+    tagList = tags.split("|")
+    return len(tagList)
+youtube["tag_count"] = youtube["tags"].apply(countTag)
 
 
 
