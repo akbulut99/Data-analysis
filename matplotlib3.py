36c33a55c217b51991b03f8a5fad800400d0bd81
akbulut99 <akbulut1199@gmail.com>
2023-01-26 14:25:15 +0300

    matplotlib3

---
+++ matplotlib3.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt 
+import numpy as np 
+x = np.arange(1,6)
+y = np.arange(2,11,2)
+
+fig,axes = plt.subplots(nrows=2,ncols=1)  #2 satırı ve 2 sütunu olan bir tuple gibi yapı oluşturduk
+
+
+fig = plt.figure(figsize=(4,4))
+axes = fig.add_axes([0,0,1,1])
+axes.plot(x,y,color = "yellow")
+
+plt.tight_layout() #grafiklerin daha güzel durmasını sağlar :)
+
+plt.show()
