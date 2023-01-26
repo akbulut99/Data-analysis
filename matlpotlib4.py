dc15fc4abb573df89d9dfed875cf38a39ab6265a
akbulut99 <akbulut1199@gmail.com>
2023-01-26 14:42:55 +0300

    matplotlib4

--- matlpotlib4.py
+++ matlpotlib4.py
@@ -7,6 +7,15 @@ axes[0].plot(x,y,color="red")
 axes[0].set_title("First Axes")
 axes[1].plot(x,x**0.5,color="purple")
 axes[1].set_title("Second Axes")
+
+fig1 = plt.figure(figsize=(6,4))
+axes1 = fig1.add_axes([0,0,1,1])
+axes1.plot(x,x**0.5,color = "red",label = "x'in karekökü")
+axes1.plot(x,x**2,color = "green",label = "x kare")
+
+axes1.plot(x,x**3,color = "blue",label = "x küp")
+axes1.legend()#labelların gözükmesi için 
 plt.tight_layout()
 plt.show()
 fig.savefig("fig1.png")
+fig1.savefig("fig2.png")
