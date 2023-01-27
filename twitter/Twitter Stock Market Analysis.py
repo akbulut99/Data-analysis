708823b0d2cb84e18a7e380cc19b8d73b7e1079a
akbulut99 <akbulut1199@gmail.com>
2023-01-27 14:43:46 +0300

    twitter

--- twitter/Twitter Stock Market Analysis.py
+++ twitter/Twitter Stock Market Analysis.py
@@ -14,4 +14,29 @@ figure = go.Figure(data=[go.Candlestick(x=tw["Date"], open=tw["Open"],high=tw["H
 figure.update_layout(title = "Twitter Stock Prices Over the Years", xaxis_rangeslider_visible=False)
 figure = px.bar(tw,x ="Date",y="Close",color="Close")
 figure.update_xaxes(rangeslider_visible=True)
+figure = px.bar(tw,x="Date",y="Close",color="Close")
+figure.update_xaxes(rangeslider_visible=True)
+figure.update_layout(title = "Twitter Stock Prices Over the Years", xaxis_rangeslider_visible=False)
+figure.update_xaxes(rangeselector = dict(buttons=list([
+            dict(count=1,label="1m",step ="month",stepmode="backward"),
+            dict(count=6,label="6m",step ="month",stepmode="backward"),
+            dict(count=3,label="3m",step ="month",stepmode="backward"),
+            dict(count=1,label="1y",step ="year",stepmode="backward"),
+            dict(count=2,label="2y",step ="year",stepmode="backward"),
+            dict(step ="all")
+
+
+])))#farklı zamanlardaki stok fiyatlarını analiz etmek için butonlar ekledik,belirli periyodları görebiliriz
+
+tw["Date"]= pd.to_datetime(tw["Date"],format='%Y-%m-%d')
+tw['Year']=tw['Date'].dt.year
+tw["Month"] = tw["Date"].dt.month
+fig = px.line(tw, 
+              x="Month", 
+              y="Close", 
+              color='Year', 
+              title="Complete Timeline of Twitter")
+
+
+
 figure.show()
